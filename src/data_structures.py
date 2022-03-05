289d5531d9fa7dc8423d5cbde130d3f1f006b63f
Nibir Baruah <nibir2893@gmail.com>
2022-03-05 15:01:03 +0530

    errors class add

--- src/data_structures.py
+++ src/data_structures.py
@@ -14,3 +14,21 @@ class SymTable:
     def set_parent(self, parent):
         self.parent = parent
 
+
+class Errors:
+
+    def __init__(self):
+        self.errors = []
+        self.n = 0
+
+    def add(self, msg, lineno):
+        error = {}
+        error['msg'] = msg
+        error['lineno'] = lineno
+        self.errors.append(error)
+        self.n += 1
+
+    def print_all(self):
+        for error in self.errors:
+            print_err = 'Error: ' + error['msg'] + ' on line ' + str(error['lineno'])
+            print(print_err)
