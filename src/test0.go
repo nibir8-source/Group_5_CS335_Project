4da9e8cefbdd77f1c18dc19823934bf00e72c060
Manish <manishyadav94220@gmail.com>
2022-03-19 16:18:10 +0530

    Constant support expanded

---
+++ src/test0.go
@@ -0,0 +1,21 @@
+package main;
+
+func main() {
+	var n, m float=20.0,30.12;
+	const PI , hello, myname = 3,4,5.11;
+	// p:=20
+	// l:=p+n;
+	// p:=60;
+	// p:=20;
+	// for i := 0; i < 4; i++{
+	// 	// fmt.Printf("GeeksforGeeks\n")  
+	// 	l:=10;
+	//   };
+	// if (p==60){
+	// 	if(m==20){
+			
+	// 	}
+	// }
+	  
+
+};
