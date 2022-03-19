55a4b3fa1436ceb9e161fd95537607077d000b55
Manish <manishyadav94220@gmail.com>
2022-03-19 17:32:00 +0530

    const-error corected

--- src/test0.go
+++ src/test0.go
@@ -2,20 +2,20 @@ package main;
 
 func main() {
 	var n, m float=20.0,30.12;
-	const PI , hello, myname = 3,4,5.11;
-	// p:=20
-	// l:=p+n;
-	// p:=60;
-	// p:=20;
-	// for i := 0; i < 4; i++{
-	// 	// fmt.Printf("GeeksforGeeks\n")  
-	// 	l:=10;
-	//   };
-	// if (p==60){
-	// 	if(m==20){
-			
-	// 	}
-	// }
+	const PI , hello, myname int= 3,4,5;
+	
+	p:=10;
+	l:=10;
+	// var p int=10;
+	
+		if(m==30.12){
+			k:="helloiitk";
+		}
+		else{
+			// k:="hello";
+			s:=10;
+		};
+	
 	  
 
 };
