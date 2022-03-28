4de9fb978c816b5fd28911950b4e7f493d8f1864
Manish <manishyadav94220@gmail.com>
2022-03-28 23:16:59 +0530

    FINAL

--- src/test0.go
+++ src/test0.go
@@ -1,24 +1,44 @@
 package main;
 
 import "fmt";
+import "math";
+import "string";
 
-var x,y int
-
-type Vertex struct {
-	Latitutde, Longitude float;
-    ac struct *Vertex
-};
-
-func calc(x,y int , z float) {
-	var a int
-	var b float
-	a = (x+y)*(x-y)
-	b = z*z*z*z*z*z
+type Address struct {
+	name string 
+	street string
+	city string
+	state string
+	Pincode int
 }
 
-func main() {
-	var vertex Vertex;
-	vertex.Latitutde=40.6833;
-	(*(vertex.ac)).Latitutde=-74.39967;
-	calc(1,2,4.0)
+const c1,c2,c3 =1,2,3
+var v1 ,v2,v3 float = 1,1.2,1.3 
+
+func main(){
+	// all type of variable Decl
+    v1=v2+v3
+	var a1 int;
+	var  a2,a3,a4 int;
+	var a5,a6 int = 10,20;
+	var a7,a8 =1,2;
+	 
+	// all constants decl
+	const a9 ;
+	const a10,a11 = "helloworld","Hopefully code work"
+	const a12, a13 int = 11,12
+
+
+	//Short Decl 
+	l:=10
+	p,q,r:=1,2,"helloworld"
+
+	//Array Decl 
+	var myarr[10]string;
+
+	//pointer decl 
+	x := 10
+	var s *int;
+	s=&x
+
 }
