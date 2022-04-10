a63d8f725be7c4afa887beddf5d586f75f5479b3
Nibir Baruah <nibir2893@gmail.com>
2022-04-10 23:25:39 +0530

    add

--- tests/Irc_tests/for_loop.go
+++ tests/Irc_tests/for_loop.go
@@ -1,29 +1,6 @@
 package main
 import "fmt"
 func main(){
-<<<<<<< HEAD
-	//Switch Case 
-
-	temp:=0
-	temp=0
-	var x [10]*int
-	hell0:=666
-	switch(2+2/2) {
-		case 3*6*4/2+3*6/8-2-2*6: 
-			var y int=1
-		case 3:
-			var z int=0	
-		default:
-			var x int =2
-	}
-
-
-
-}
-
-
-
-=======
     var a int= 34<<45
     i:=0
     for {
@@ -38,4 +15,3 @@ func main(){
         i++
     }
 }
->>>>>>> 97a486935529c8cfd6fbe85e100471367a33a2c7
