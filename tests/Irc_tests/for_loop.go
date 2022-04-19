94d2e617236ca09224e8ec632162d0e19be84213
Nibir Baruah <nibir2893@gmail.com>
2022-04-19 16:32:15 +0530

    add

--- tests/Irc_tests/for_loop.go
+++ tests/Irc_tests/for_loop.go
@@ -1,17 +1,11 @@
 package main
 import "fmt"
-func main(){
-    var a int= 34<<45
-    i:=0
-    for {
-		for j:=0; j<10; j++ {
-			j++
-		}
 
-        if (56|i) < 78{
-            continue
-        }
-        a=a+(56|i)
-        i++
-    }
+var e int = 5
+var d int = 6
+
+func main(){
+    var a int = 34
+    var b int = 23
+    c := e + d
 }
