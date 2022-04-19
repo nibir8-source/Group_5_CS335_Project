0031562cf9c549aa0dce9e70d58f0811b9fcf320
Manish <manishyadav94220@gmail.com>
2022-04-19 16:35:11 +0530

    helloworld

--- tests/Ast_tests/test2.go
+++ tests/Ast_tests/test2.go
@@ -1,21 +1,21 @@
 package main
 import "fmt"
-func factorial(n int) (int) {
-    if (n == 0 || n == 1) {
-        return 1
-    }    
-    if n < 0 {
-        return -1
-    }
-    return n*factorial(n - 1)
-}
+// func factorial(n int) (int) {
+//     if (n == 0 || n == 1) {
+//         return 1
+//     }    
+//     if n < 0 {
+//         return -1
+//     }
+//     return n*factorial(n - 1)
+// }
 
 func sum(p,q int)(int){
 	return p+q
 }
 
 func main(){
-   var l int =factorial(10)
+//    var l int =factorial(10)
    //All types of expression
    var e1,e2,e3,e4,e5,e6,e7,e8 = 10,11,12,13,14,15,16,17
    e1=e1+e2
