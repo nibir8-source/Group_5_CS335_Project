97195b21fbd0af717c313f44402c527b20ab4f6f
Manish <manishyadav94220@gmail.com>
2022-04-25 21:32:30 +0530

    Pointer testcase added

---
+++ tests/final_tests/if_else.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+func main(){
+	var a=10;
+	var b=10;
+	if(a==b){
+		print(a,b)
+		if(a+b > 30){
+			l:=10
+			print(l)
+		}
+	}
+	else{
+		c:=a+b
+		print(c)
+	}
+
+}
+
