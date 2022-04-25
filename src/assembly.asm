493f79be76bcdcf9e4440053c2da2c001d643f93
Nibir Baruah <nibir2893@gmail.com>
2022-04-25 22:48:44 +0530

    final

--- src/assembly.asm
+++ src/assembly.asm
@@ -15,111 +15,55 @@ section .text
 main:
     push ebp
     mov ebp, esp
-    sub esp, 144
-    mov edi, 0
-    mov [ebp-84], edi
-label_no_0:
-label_no_2:
-    mov edi, [ebp-84]
-    mov esi, 10
-    xor eax, eax
-    cmp edi, esi
-    setl al
-    mov [ebp-88], eax
-    mov edi, [ebp-88]
-    cmp edi, 0
-    je label_no_3
-    mov edi, [ebp-84]
-    mov [ebp-92], edi
-    mov edi, [ebp-92]
-    mov esi, 8
-    imul edi, esi
-    mov [ebp-92], edi
-    lea edi, [ebp-4]
-    mov esi, [ebp-92]
-    sub edi, esi
-    mov [ebp-92], edi
-    mov edi, [ebp-92]
-    mov esi, 0
+    sub esp, 16
+    mov edi, 23
+    mov [ebp-0], edi
+    mov edi, 45
+    mov [ebp-4], edi
+    mov edi, [ebp-0]
+    mov esi, [ebp-4]
     add edi, esi
-    mov [ebp-96], edi
-    mov edi, [ebp-84]
-    mov esi, 10
-    add edi, esi
-    mov [ebp-100], edi
-    mov edi, [ebp-100]
-    mov esi, [ebp-96]
-    mov [esi], edi
-    mov edi, [ebp-84]
-    mov [ebp-104], edi
-    mov edi, [ebp-104]
-    mov esi, 8
-    imul edi, esi
-    mov [ebp-104], edi
-    lea edi, [ebp-4]
-    mov esi, [ebp-104]
-    sub edi, esi
-    mov [ebp-104], edi
-    mov edi, [ebp-104]
-    mov esi, 4
-    add edi, esi
-    mov [ebp-108], edi
-    mov edi, 30
-    mov esi, [ebp-108]
-    mov [esi], edi
-    mov edi, [ebp-84]
-    mov [ebp-112], edi
-    mov edi, [ebp-112]
-    mov esi, 8
-    imul edi, esi
-    mov [ebp-112], edi
-    lea edi, [ebp-4]
-    mov esi, [ebp-112]
-    sub edi, esi
-    mov [ebp-112], edi
-    mov edi, [ebp-112]
-    mov esi, 0
-    add edi, esi
-    mov [ebp-116], edi
-    mov edi, [ebp-84]
-    mov [ebp-120], edi
-    mov edi, [ebp-120]
-    mov esi, 8
-    imul edi, esi
-    mov [ebp-120], edi
-    lea edi, [ebp-4]
-    mov esi, [ebp-120]
-    sub edi, esi
-    mov [ebp-120], edi
-    mov edi, [ebp-120]
-    mov esi, 4
-    add edi, esi
-    mov [ebp-124], edi
-    mov edi, [ebp-116]
-    mov edi, [edi]
-    mov [ebp-128], edi
-    mov edi, [ebp-124]
-    mov edi, [edi]
-    mov [ebp-132], edi
-    mov edi, [ebp-128]
-    mov esi, [ebp-132]
-    add edi, esi
-    mov [ebp-136], edi
-    mov edi, [ebp-136]
-    mov [ebp-140], edi
-    mov esi, [ebp-140]
+    mov [ebp-4], edi
+    mov edx, [ebp-0]
+    mov eax, 2
+    mov ecx, eax
+    sal edx, cl
+    mov [ebp-8], edx
+    mov edx, [ebp-4]
+    mov eax, 2
+    mov ecx, eax
+    sar edx, cl
+    mov [ebp-12], edx
+    mov esi, [ebp-0]
     push esi
     push print_int
     call printf
     pop esi
     pop esi
-    mov edi, [ebp-84]
-    mov esi, 1
-    add edi, esi
-    mov [ebp-84], edi
-    jmp label_no_2
-label_no_3:
-label_no_1:
+    mov esi, [ebp-4]
+    push esi
+    push print_int
+    call printf
+    pop esi
+    pop esi
+    mov esi, [ebp-4]
+    push esi
+    push print_int
+    call printf
+    pop esi
+    pop esi
+    mov esi, [ebp-8]
+    push esi
+    push print_int
+    call printf
+    pop esi
+    pop esi
+    mov esi, [ebp-12]
+    push esi
+    push print_int
+    call printf
+    pop esi
+    pop esi
     mov esp, ebp
     pop ebp
     ret
--- src/test.go
+++ src/test.go
@@ -1,19 +1,10 @@
 package main
-import "math"
-type def struct{
-	a,b int;
-};
+import "main"
+
+var a, b int
 
 func main(){
-	var arr[10] def;
-	for i:=0; i<10;i++{
-		arr[i].a = i+10
-		arr[i].b = 30
-		c := arr[i].a + arr[i].b
-		print(c)
-	}
-};
-
-
-
-
+	a = 23
+	b = 45
+	print(a,b,a + b, a << 2, b >> 2)
+}
